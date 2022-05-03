1c0fa85f3ee7c63910354cb729cd80f94daf5540
henri2h <henri.carnot@hotmail.fr>
2022-05-03 16:01:43 +0200

    feat: added more debug tries

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -29,13 +29,36 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  Future<void> displayLoadingModalWithError() async {
+    await showFutureLoadingDialog(
+      context: context,
+      future: () async {
+        await Future.delayed(const Duration(seconds: 1));
+        throw Exception("hehe, i'm gonna kill your app");
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Home")),
       body: Center(
-          child: ElevatedButton(
-              child: const Text("Press me"), onPressed: displayLoadingModal)),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+                child: const Text("Future loading"),
+                onPressed: displayLoadingModal),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                child: const Text("FutureLoadingWithError"),
+                onPressed: displayLoadingModalWithError),
+          ],
+        ),
+      ),
     );
   }
 }
